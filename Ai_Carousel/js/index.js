0892bf6b6df4838c22ab0a9dc2ddc15f92471a1b
AtaOta <ataota.org@gmail.com>
2021-11-10 13:03:53 +0530

    Toggler Navigation-Button Added, VERSION-1.1.4

--- Ai_Carousel/js/index.js
+++ Ai_Carousel/js/index.js
@@ -7,7 +7,7 @@ if (Slider_Wrapper[0] != undefined){
         let timeOutFunctionId = "";
         let InterValId = "";
         let Carouse_Moving = 'Moving';
-        // let ParentElm = element.parentElement;
+        let ParentElm = element.parentElement;
         let CMD = -1; /* Carousel Movement Direction */
         let IsTuchDown = false;
         let startX;
@@ -34,11 +34,44 @@ if (Slider_Wrapper[0] != undefined){
                 setTimeout(()=>{
                     CW.style.transition = 'all 0.5s ease-in-out 0.5s';
                 })
+
+                counterParentElm = element.parentElement.children[1]
+                if (counterParentElm != undefined){
+                    let counterNext;
+                    let counterPrevious;
+                    if(CMD == -1){
+                        let el = CW.children[0].classList[0]
+                        counterNext = counterParentElm.children[el]
+                        counterPrevious = counterParentElm.children[el-1]
+                    }
+                    if(CMD == 1){
+                        let el = CW.children[1].classList[0]
+                        counterNext = counterParentElm.children[el-1]
+                        counterPrevious = counterParentElm.children[el]
+                    }
+                    if (counterNext != undefined){
+                        counterNext.classList.add('Slider_Button_Active');
+                    }
+                    if(counterPrevious != undefined){
+                        counterPrevious.classList.remove('Slider_Button_Active');
+                    }
+                    
+                    if(counterNext == undefined & CMD == -1){
+                        counterParentElm.lastElementChild.classList.remove('Slider_Button_Active');
+                        counterParentElm.firstElementChild.classList.add('Slider_Button_Active');
+                    }
+                    if(counterPrevious == undefined & CMD == 1){
+                        counterParentElm.lastElementChild.classList.add('Slider_Button_Active');
+                        // console.warn(counterParentElm.firstElementChild)
+                        counterParentElm.firstElementChild.classList.remove('Slider_Button_Active');
+                    }
+                }
             });
         }
         TransitionFunction();
 
         const Carousel = ()=>{
+            ParentElm = element.parentElement;
             // CONTENT WRAPPER HOLDER (CWHolder)
             let CWHolder = element.firstElementChild; 
             let screenWidth = element.clientWidth;
@@ -95,15 +128,17 @@ if (Slider_Wrapper[0] != undefined){
                 const x = touch.pageX - element.offsetLeft;
                 const slidMove = x - startX;
                 let move = CNTWidth + gap_between_CNT
-                if(slidMove<0){
-                    CWHolder.style.justifyContent = 'flex-end';
-                    CW.style.transform = `translatex(${-move}px)`;
-                    CMD = -1
-                }
-                if(slidMove>0){
-                    CWHolder.style.justifyContent = 'flex-end';
-                    CW.style.transform = `translatex(${move}px)`;
-                    CMD = 1
+                if(CWwidth > screenWidth){
+                    if(slidMove<0){
+                        CWHolder.style.justifyContent = 'flex-end';
+                        CW.style.transform = `translatex(${-move}px)`;
+                        CMD = -1
+                    }
+                    if(slidMove>0){
+                        CWHolder.style.justifyContent = 'flex-start';
+                        CW.style.transform = `translatex(${move}px)`;
+                        CMD = 1
+                    }
                 }
             }, { passive: true })
 
@@ -133,27 +168,77 @@ if (Slider_Wrapper[0] != undefined){
                         CMD = 1
                     }
                     let move = CNTWidth + gap_between_CNT
-                    CWHolder.style.justifyContent = 'flex-end';
+                    CWHolder.style.justifyContent = 'flex-start';
                     CW.style.transform = `translatex(${move}px)`;
                 })
                 NVbtnNext.addEventListener('click', ()=>{
                     CMD = -1
                     let move = CNTWidth + gap_between_CNT
-                    CWHolder.style.justifyContent = 'flex-start';
+                    CWHolder.style.justifyContent = 'flex-end';
                     CW.style.transform = `translatex(${-move}px)`;
                 })
             }
 
+            // ADDING TOGGLER NAVIGATION BUTTON
+            const createMultiNavBtn = () => {
+                let counter_click = document.createElement('div')
+                counter_click.className = `counter_click`;
+                if ((ParentElm.children.length == 1)){
+                    for(let i = 0; i < totalCNT; i++) {
+                        let CarouselClass = CW.children[i].classList[0];
+                        if(isNaN(CarouselClass) == true){
+                            CW.children[i].className = (`${i+1} ${CarouselClass}`)
+                        }
+                        let new_div = document.createElement('div');
+                        new_div.innerText = (i+1)
+                        new_div.className = `Slider_Button`;
+                        counter_click.appendChild(new_div);
+
+                        // if (i==1){
+                        //     new_div.classList.add('Slider_Button_Active')
+                        // }
+                        
+                        // // if (CNTWidth > screenWidth){
+                        // //     CW.children[i].style.maxWidth = `${screenWidth}px`;
+                        // //     CWHolder.style.transform = `translatex(${-(screenWidth+gap_between_CNT)}px)`;
+                        // // }
+                        // // if((CNTdisplaing<=2) & (totalCNT==2) & ((2*CNTWidth+gap_between_CNT) > screenWidth)){
+                        // //     move = (screenWidth-CNTWidth)/2
+                        // //     CWHolder.style.maxWidth = `${CNTWidth}px`;
+                        // //     CWHolder.style.transform = `translatex(${move}px)`;
+                        // //     CW.children[i].style.minWidth = `${CNTWidth}px`;
+                        // // }
+                    }
+                    if (totalCNT > 2){
+                        ParentElm.appendChild(counter_click);
+                    }
+                }
+
+            }
+
+            // ON-RESIZE THE WINDOW RESIZE THOSE CONTENT
             window.addEventListener('resize', ()=>{
                 clearTimeout(timeOutFunctionId);
                 timeOutFunctionId = "";
                 clearInterval(InterValId);
                 InterValId = "";
+                if(CWwidth <= screenWidth){
+                    if(element.children.length>1){
+                        element.removeChild(element.children[1])
+                        element.removeChild(element.children[1])
+                    }
+                }
                 timeOutFunctionId = setTimeout(Carousel, 3000);
             })
-            if(CWwidth > screenWidth){
+
+            if(CWwidth < screenWidth){
                 CWHolder.style.transform = `none`;
-                CW.style.transform = `none`; 
+                CW.style.transform = `none`;
+                CWHolder.style.justifyContent = 'center';
+                
+                if ((totalCNT > 2) & (ParentElm.children[1] != undefined)){
+                    ParentElm.removeChild(ParentElm.children[1])
+                }
             }
             if(CWwidth > screenWidth){
                 CWHolder.style.transform = `translatex(${-xMove}px)`;
@@ -161,6 +246,7 @@ if (Slider_Wrapper[0] != undefined){
                 InterValId = "";
                 if (!InterValId) InterValId = setInterval(moveCarousel, sliding_duration);
                 addNevigationButton();
+                createMultiNavBtn();
             }
             if((CNTdisplaing==2) & (totalCNT>2)){
                 move = (CNTWidth-((screenWidth-CNTWidth)/2))+gap_between_CNT
